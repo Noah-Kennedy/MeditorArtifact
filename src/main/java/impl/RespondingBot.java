33ce6ef854faf4bf7bfd8e850b3fd373d3b39b7d
Noah Kennedy <Nomaxx117@gmail.com>
2019-02-13 20:24:28 -0600

    Added ChatBot interface

---
+++ src/main/java/impl/RespondingBot.java
@@ -0,0 +1,35 @@
+package impl;
+
+import interfaces.ChatBot;
+import interfaces.Mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RespondingBot implements ChatBot {
+    private String message;
+    private String username;
+    private Mediator mediator;
+
+    @Override
+    public void setScamMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void spam() {
+        findUnfortunateSouls()
+                .stream()
+                .map(idiot -> new Message(message, idiot, username))
+                .forEach(mediator::postMessage);
+    }
+
+    private List<String> findUnfortunateSouls() {
+        return mediator
+                .getMessages(username)
+                .stream()
+                .map(Message::getSender)
+                .collect(Collectors.toList());
+    }
+}
